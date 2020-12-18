20845011a3221c0ac775909f9ac97bce771c1908
devansh gupta <devansh.gupta@students.iiit.ac.in>
2020-12-18 18:37:14 +0530

    rest api files added.

--- fyle/fyleRest/fyleRest/settings.py
+++ fyle/fyleRest/fyleRest/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
